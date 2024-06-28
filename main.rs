51d245f797569d675991151dc9538317cc3be75f
saent-x <torsxperience@outlook.com>
2024-06-29 00:29:43 +0100

    I: vectors

--- main.rs
+++ main.rs
@@ -19,7 +19,64 @@ fn main(){
     //
     // println!("End value: {val}")
 
-    string_s()
+    vectors()
+}
+
+fn vectors(){
+    let vec_1: Vec<u32> = Vec::new();
+    let mut vec_2: Vec<u32> =vec![1,2,3,4,5];
+
+    vec_2.push(6);
+
+    println!("first: {}", vec_2[0]);
+    let last: &u32 = &vec_2[5];
+
+    match vec_2.get(5){
+        Some(last) => println!("last: {last}"),
+        None => println!("empty!"),
+    }
+
+    for i in &mut vec_2{
+        *i *= 2;
+    }
+    for i in &vec_2{
+        println!("{}", i);
+    }
+
+    println!("vec len: {}", vec_2.len());
+    println!("pop: {:?}", vec_2.pop());
+}
+
+fn type_casting_enums(){
+    // type-casting
+    let int_u8: u8 =5;
+    let int_2_u8: u8 = 4;
+
+    let int_4_u32: u32 = (int_u8 as u32) + (int_2_u8 as u32);
+
+    // enums
+
+    enum Birds {
+        Falcon,
+        Eagle,
+        Pigeon,
+        Vulture,
+        Dove
+    }
+
+    impl Birds {
+        fn is_wild(&self) -> bool {
+            match self {
+                Birds::Vulture | Birds::Eagle => true,
+                _ => false,
+            }
+        }
+    }
+
+    let bird: Birds = Birds::Vulture;
+    let ans = if bird.is_wild() { "yes" }else { "no"};
+
+    println!("wild? {}", ans);
 }
 
 fn datatypes_operators_keywords(){
