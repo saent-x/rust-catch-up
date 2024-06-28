57250cb7579330b6c9918d3c1700646cb65bce71
saent-x <torsxperience@outlook.com>
2024-06-28 04:24:28 +0100

    I: loops variables and datatypes, shadowing

--- main.rs
+++ main.rs
@@ -1,4 +1,36 @@
 
 fn main(){
-    println!("hello rust")
+    loops_var_dt()
+}
+
+// loops variables and datatypes
+fn loops_var_dt(){
+    let mut counter: i64 = 0;
+    const LANG: &str = "rust";
+
+    loop {
+        if counter == 100 {
+            break;
+        }
+
+        println!("hello rust -> counter is {counter}");
+        counter += 1;  
+    }
+}
+
+fn shadowing(value: i32) -> i32{
+    let x = value;
+
+    let x = x * 5;
+
+    {
+        let sub: i32 = x - 1;
+        let x: i32 = x - sub;
+
+        println!("x value in shadow: {x}");
+    }
+
+    println!("x value outside shadow: {x}");
+
+    x
 }
