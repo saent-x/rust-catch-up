3f8708d106ecf4705a071450811d9e52c8ffd8fc
saent-x <torsxperience@outlook.com>
2024-06-28 22:39:20 +0100

    I: datatypes, operators and keywords

--- main.rs
+++ main.rs
@@ -1,6 +1,47 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
 
 fn main(){
-    loops_var_dt()
+    // print!("Start Value: ");
+    //
+    // let mut value =  String::new();
+    //
+    // io::stdin()
+    //     .read_line(&mut value)
+    //     .expect("failed to real line");
+    //
+    // let val: i32 = shadowing(10);
+    //
+    // println!("End value: {val}")
+
+    datatypes_operators_keywords();
+}
+
+fn datatypes_operators_keywords(){
+    let arr_1 = [1,2,3,4,5,6,7,8,9,10];
+    let mut loop_idx = 0;
+
+    // loop keyword
+    loop{
+        if arr_1[loop_idx] % 2 == 0 {
+            loop_idx += 1;
+
+            continue;
+        }
+        if arr_1[loop_idx] == 9 {
+            break;
+        }
+
+        println!("val: {}", arr_1[loop_idx]);
+        loop_idx += 1;
+    }
+
+
 }
 
 // loops variables and datatypes
